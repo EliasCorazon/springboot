d9a610c0de058a08b12fcb47d2bf933250e7a637
EliasCorazon <102911055+EliasCorazon@users.noreply.github.com>
2022-07-06 18:12:58 +0500

    first

--- PPBoot/src/main/java/com/example/service/UserServiceImp.java
+++ PPBoot/src/main/java/com/example/service/UserServiceImp.java
@@ -4,6 +4,7 @@ import com.example.model.User;
 import com.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -13,22 +14,22 @@ public class UserServiceImp implements UserService {
     public UserServiceImp(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+    @Transactional
     @Override
     public List<User> allUsers() {
         return userRepository.findAll();
     }
-
+    @Transactional
     @Override
     public User findById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
-
+    @Transactional
     @Override
     public User addUser(User addUser) {
         return userRepository.save(addUser);
     }
-
+    @Transactional
     @Override
     public void delete(Long id) {
         userRepository.deleteById(id);
